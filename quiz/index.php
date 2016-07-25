a733fc10209fa88e7d0fea01572ba4c02efcfed8
Nikki <nicleemart@gmail.com>
2016-07-25 15:53:46 -0500

    created form

---
+++ quiz/index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Single Page Quiz</title>
+
+		<script type="text/javascript" src="global.js"></script>
+	</head>
+
+	<body>
+		<form>
+			<input type="radio" name="one">One<br>
+			<input type="radio" name="two">Two<br>
+			<input type="radio" name="three">Three<br>
+			<input type="submit" value="Submit">
+		</form>
+	</body>
+</html>
